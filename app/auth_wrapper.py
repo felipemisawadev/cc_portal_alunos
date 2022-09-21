87ce394c4f5d6eb1c6d20a3cedf5c846d264340b
felipemisawadev <felipemisawadev@gmail.com>
2022-09-21 20:44:16 +0200

    remove commented sections

--- app/auth_wrapper.py
+++ app/auth_wrapper.py
@@ -2,21 +2,6 @@ from flask import session, url_for, redirect, g
 import functools
 
 
-# def auth_required(func):
-#     """Decorator that reports the execution time."""
-
-#     def auth_wrapper(*args, **kwargs):
-#         if "token" in session:
-#             result = func(*args, **kwargs)
-#             return result
-#         else:
-#             return redirect(url_for("sign_in"))
-
-#     # Renaming the function name:
-#     auth_wrapper.__name__ = func.__name__
-#     return auth_wrapper
-
-
 def auth_required(aws_auth):
     def actual_decorator(func):
         @functools.wraps(func)
